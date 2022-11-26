38f83d5d7808cc9f6c7f6d43daad6ea6ae70f303
Alexandra Romano <alex.romano80@gmail.com>
2022-11-26 14:45:07 -0700

    commit tree 7926394d8151492c7a590c04e79604e24ff3cfc8

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+element.classList.remove("main#main");
+
+document.body.append(h1);
+const newHeader = document.createElement("h1");
+for (let i = 0; i < 3; i++) {
+    const victory = document.createElement("victory");
+    victory.textContent = (i + 1).toString();
+    ul.append(victory);
+  }
+element.append(h1);
+
+const main = document.getElementById("main");
+main.innerHTML = "<h1>Lexi</h1><h3>is the champion</h3>";
+
+
